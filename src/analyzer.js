9555f2cc90faafec68a9d76d61c02949e2a40735
Akash Beh <abeh1@lion.lmu.edu>
2025-03-25 16:35:13 -0700

    Copied from toal

--- src/analyzer.js
+++ src/analyzer.js
@@ -243,4 +243,441 @@ export default function analyze(match) {
     const message = `${paramCount} argument(s) required but ${argCount} passed`
     must(argCount === paramCount, message, at)
   }
+  
+  
+  
+  
+  const builder = match.matcher.grammar.createSemantics().addOperation("rep", {
+    Program(statements) {
+      return core.program(statements.children.map(s => s.rep()))
+    },
+
+    VarDecl(modifier, id, _eq, exp, _semicolon) {
+      mustNotAlreadyBeDeclared(id.sourceString, { at: id })
+      const initializer = exp.rep()
+      const mutable = modifier.sourceString === "let"
+      const variable = core.variable(id.sourceString, mutable, initializer.type)
+      context.add(id.sourceString, variable)
+      return core.variableDeclaration(variable, initializer)
+    },
+
+    TypeDecl(_struct, id, _left, fields, _right) {
+      mustNotAlreadyBeDeclared(id.sourceString, { at: id })
+      // To allow recursion, enter into context without any fields yet
+      const type = core.structType(id.sourceString, [])
+      context.add(id.sourceString, type)
+      // Now add the types as you parse and analyze. Since we already added
+      // the struct type itself into the context, we can use it in fields.
+      type.fields = fields.children.map(field => field.rep())
+      mustHaveDistinctFields(type, { at: id })
+      mustNotBeSelfContaining(type, { at: id })
+      return core.typeDeclaration(type)
+    },
+
+    Field(id, _colon, type) {
+      return core.field(id.sourceString, type.rep())
+    },
+
+    FunDecl(_fun, id, parameters, _colons, type, block) {
+      mustNotAlreadyBeDeclared(id.sourceString, { at: id })
+      // Add immediately so that we can have recursion
+      const fun = core.fun(id.sourceString)
+      context.add(id.sourceString, fun)
+
+      // Parameters are part of the child context
+      context = context.newChildContext({ inLoop: false, function: fun })
+      fun.params = parameters.rep()
+
+      // Now that the parameters are known, we compute the function's type.
+      // This is fine; we did not need the type to analyze the parameters,
+      // but we do need to set it before analyzing the body.
+      const paramTypes = fun.params.map(param => param.type)
+      const returnType = type.children?.[0]?.rep() ?? core.voidType
+      fun.type = core.functionType(paramTypes, returnType)
+
+      // Analyze body while still in child context
+      fun.body = block.rep()
+
+      // Go back up to the outer context before returning
+      context = context.parent
+      return core.functionDeclaration(fun)
+    },
+
+    Params(_open, paramList, _close) {
+      // Returns a list of variable nodes
+      return paramList.asIteration().children.map(p => p.rep())
+    },
+
+    Param(id, _colon, type) {
+      const param = core.variable(id.sourceString, false, type.rep())
+      mustNotAlreadyBeDeclared(param.name, { at: id })
+      context.add(param.name, param)
+      return param
+    },
+
+    Type_optional(baseType, _questionMark) {
+      return core.optionalType(baseType.rep())
+    },
+
+    Type_array(_left, baseType, _right) {
+      return core.arrayType(baseType.rep())
+    },
+
+    Type_function(_left, types, _right, _arrow, type) {
+      const paramTypes = types.asIteration().children.map(t => t.rep())
+      const returnType = type.rep()
+      return core.functionType(paramTypes, returnType)
+    },
+
+    Type_id(id) {
+      const entity = context.lookup(id.sourceString)
+      mustHaveBeenFound(entity, id.sourceString, { at: id })
+      mustBeAType(entity, { at: id })
+      return entity
+    },
+
+    Statement_bump(exp, operator, _semicolon) {
+      const variable = exp.rep()
+      mustHaveIntegerType(variable, { at: exp })
+      return operator.sourceString === "++"
+        ? core.increment(variable)
+        : core.decrement(variable)
+    },
+
+    Statement_assign(variable, _eq, expression, _semicolon) {
+      const source = expression.rep()
+      const target = variable.rep()
+      mustBeMutable(target, { at: variable })
+      mustBeAssignable(source, { toType: target.type }, { at: variable })
+      return core.assignment(target, source)
+    },
+
+    Statement_call(call, _semicolon) {
+      return call.rep()
+    },
+
+    Statement_break(breakKeyword, _semicolon) {
+      mustBeInLoop({ at: breakKeyword })
+      return core.breakStatement
+    },
+
+    Statement_return(returnKeyword, exp, _semicolon) {
+      mustBeInAFunction({ at: returnKeyword })
+      mustReturnSomething(context.function, { at: returnKeyword })
+      const returnExpression = exp.rep()
+      mustBeReturnable(returnExpression, { from: context.function }, { at: exp })
+      return core.returnStatement(returnExpression)
+    },
+
+    Statement_shortreturn(returnKeyword, _semicolon) {
+      mustBeInAFunction({ at: returnKeyword })
+      mustNotReturnAnything(context.function, { at: returnKeyword })
+      return core.shortReturnStatement
+    },
+
+    IfStmt_long(_if, exp, block1, _else, block2) {
+      const test = exp.rep()
+      mustHaveBooleanType(test, { at: exp })
+      context = context.newChildContext()
+      const consequent = block1.rep()
+      context = context.parent
+      context = context.newChildContext()
+      const alternate = block2.rep()
+      context = context.parent
+      return core.ifStatement(test, consequent, alternate)
+    },
+
+    IfStmt_elsif(_if, exp, block, _else, trailingIfStatement) {
+      const test = exp.rep()
+      mustHaveBooleanType(test, { at: exp })
+      context = context.newChildContext()
+      const consequent = block.rep()
+      context = context.parent
+      const alternate = trailingIfStatement.rep()
+      return core.ifStatement(test, consequent, alternate)
+    },
+
+    IfStmt_short(_if, exp, block) {
+      const test = exp.rep()
+      mustHaveBooleanType(test, { at: exp })
+      context = context.newChildContext()
+      const consequent = block.rep()
+      context = context.parent
+      return core.shortIfStatement(test, consequent)
+    },
+
+    LoopStmt_while(_while, exp, block) {
+      const test = exp.rep()
+      mustHaveBooleanType(test, { at: exp })
+      context = context.newChildContext({ inLoop: true })
+      const body = block.rep()
+      context = context.parent
+      return core.whileStatement(test, body)
+    },
+
+    LoopStmt_repeat(_repeat, exp, block) {
+      const count = exp.rep()
+      mustHaveIntegerType(count, { at: exp })
+      context = context.newChildContext({ inLoop: true })
+      const body = block.rep()
+      context = context.parent
+      return core.repeatStatement(count, body)
+    },
+
+    LoopStmt_range(_for, id, _in, exp1, op, exp2, block) {
+      const [low, high] = [exp1.rep(), exp2.rep()]
+      mustHaveIntegerType(low, { at: exp1 })
+      mustHaveIntegerType(high, { at: exp2 })
+      const iterator = core.variable(id.sourceString, false, core.intType)
+      context = context.newChildContext({ inLoop: true })
+      context.add(id.sourceString, iterator)
+      const body = block.rep()
+      context = context.parent
+      return core.forRangeStatement(iterator, low, op.sourceString, high, body)
+    },
+
+    LoopStmt_collection(_for, id, _in, exp, block) {
+      const collection = exp.rep()
+      mustHaveAnArrayType(collection, { at: exp })
+      const iterator = core.variable(id.sourceString, false, collection.type.baseType)
+      context = context.newChildContext({ inLoop: true })
+      context.add(iterator.name, iterator)
+      const body = block.rep()
+      context = context.parent
+      return core.forStatement(iterator, collection, body)
+    },
+
+    Block(_open, statements, _close) {
+      // No need for a block node, just return the list of statements
+      return statements.children.map(s => s.rep())
+    },
+
+    Exp_conditional(exp, _questionMark, exp1, colon, exp2) {
+      const test = exp.rep()
+      mustHaveBooleanType(test, { at: exp })
+      const [consequent, alternate] = [exp1.rep(), exp2.rep()]
+      mustBothHaveTheSameType(consequent, alternate, { at: colon })
+      return core.conditional(test, consequent, alternate, consequent.type)
+    },
+
+    Exp1_unwrapelse(exp1, elseOp, exp2) {
+      const [optional, op, alternate] = [exp1.rep(), elseOp.sourceString, exp2.rep()]
+      mustHaveAnOptionalType(optional, { at: exp1 })
+      mustBeAssignable(alternate, { toType: optional.type.baseType }, { at: exp2 })
+      return core.binary(op, optional, alternate, optional.type)
+    },
+
+    Exp2_or(exp, _ops, exps) {
+      let left = exp.rep()
+      mustHaveBooleanType(left, { at: exp })
+      for (let e of exps.children) {
+        let right = e.rep()
+        mustHaveBooleanType(right, { at: e })
+        left = core.binary("||", left, right, core.booleanType)
+      }
+      return left
+    },
+
+    Exp2_and(exp, _ops, exps) {
+      let left = exp.rep()
+      mustHaveBooleanType(left, { at: exp })
+      for (let e of exps.children) {
+        let right = e.rep()
+        mustHaveBooleanType(right, { at: e })
+        left = core.binary("&&", left, right, core.booleanType)
+      }
+      return left
+    },
+
+    Exp3_bitor(exp, _ops, exps) {
+      let left = exp.rep()
+      mustHaveIntegerType(left, { at: exp })
+      for (let e of exps.children) {
+        let right = e.rep()
+        mustHaveIntegerType(right, { at: e })
+        left = core.binary("|", left, right, core.intType)
+      }
+      return left
+    },
+
+    Exp3_bitxor(exp, xorOps, exps) {
+      let left = exp.rep()
+      mustHaveIntegerType(left, { at: exp })
+      for (let e of exps.children) {
+        let right = e.rep()
+        mustHaveIntegerType(right, { at: e })
+        left = core.binary("^", left, right, core.intType)
+      }
+      return left
+    },
+
+    Exp3_bitand(exp, andOps, exps) {
+      let left = exp.rep()
+      mustHaveIntegerType(left, { at: exp })
+      for (let e of exps.children) {
+        let right = e.rep()
+        mustHaveIntegerType(right, { at: e })
+        left = core.binary("&", left, right, core.intType)
+      }
+      return left
+    },
+
+    Exp4_compare(exp1, relop, exp2) {
+      const [left, op, right] = [exp1.rep(), relop.sourceString, exp2.rep()]
+      // == and != can have any operand types as long as they are the same
+      // But inequality operators can only be applied to numbers and strings
+      if (["<", "<=", ">", ">="].includes(op)) {
+        mustHaveNumericOrStringType(left, { at: exp1 })
+      }
+      mustBothHaveTheSameType(left, right, { at: relop })
+      return core.binary(op, left, right, core.booleanType)
+    },
+
+    Exp5_shift(exp1, shiftOp, exp2) {
+      const [left, op, right] = [exp1.rep(), shiftOp.sourceString, exp2.rep()]
+      mustHaveIntegerType(left, { at: exp1 })
+      mustHaveIntegerType(right, { at: exp2 })
+      return core.binary(op, left, right, core.intType)
+    },
+
+    Exp6_add(exp1, addOp, exp2) {
+      const [left, op, right] = [exp1.rep(), addOp.sourceString, exp2.rep()]
+      if (op === "+") {
+        mustHaveNumericOrStringType(left, { at: exp1 })
+      } else {
+        mustHaveNumericType(left, { at: exp1 })
+      }
+      mustBothHaveTheSameType(left, right, { at: addOp })
+      return core.binary(op, left, right, left.type)
+    },
+
+    Exp7_multiply(exp1, mulOp, exp2) {
+      const [left, op, right] = [exp1.rep(), mulOp.sourceString, exp2.rep()]
+      mustHaveNumericType(left, { at: exp1 })
+      mustBothHaveTheSameType(left, right, { at: mulOp })
+      return core.binary(op, left, right, left.type)
+    },
+
+    Exp8_power(exp1, powerOp, exp2) {
+      const [left, op, right] = [exp1.rep(), powerOp.sourceString, exp2.rep()]
+      mustHaveNumericType(left, { at: exp1 })
+      mustBothHaveTheSameType(left, right, { at: powerOp })
+      return core.binary(op, left, right, left.type)
+    },
+
+    Exp8_unary(unaryOp, exp) {
+      const [op, operand] = [unaryOp.sourceString, exp.rep()]
+      let type
+      if (op === "#") {
+        mustHaveAnArrayType(operand, { at: exp })
+        type = core.intType
+      } else if (op === "-") {
+        mustHaveNumericType(operand, { at: exp })
+        type = operand.type
+      } else if (op === "!") {
+        mustHaveBooleanType(operand, { at: exp })
+        type = core.booleanType
+      } else if (op === "some") {
+        type = core.optionalType(operand.type)
+      } else if (op === "random") {
+        mustHaveAnArrayType(operand, { at: exp })
+        type = operand.type.baseType
+      }
+      return core.unary(op, operand, type)
+    },
+
+    Exp9_emptyarray(ty, _open, _close) {
+      const type = ty.rep()
+      mustBeAnArrayType(type, { at: ty })
+      return core.emptyArray(type)
+    },
+
+    Exp9_arrayexp(_open, args, _close) {
+      const elements = args.asIteration().children.map(e => e.rep())
+      mustAllHaveSameType(elements, { at: args })
+      return core.arrayExpression(elements)
+    },
+
+    Exp9_emptyopt(_no, type) {
+      return core.emptyOptional(type.rep())
+    },
+
+    Exp9_parens(_open, expression, _close) {
+      return expression.rep()
+    },
+
+    Exp9_subscript(exp1, _open, exp2, _close) {
+      const [array, subscript] = [exp1.rep(), exp2.rep()]
+      mustHaveAnArrayType(array, { at: exp1 })
+      mustHaveIntegerType(subscript, { at: exp2 })
+      return core.subscript(array, subscript)
+    },
+
+    Exp9_member(exp, dot, id) {
+      const object = exp.rep()
+      let structType
+      if (dot.sourceString === "?.") {
+        mustHaveAnOptionalStructType(object, { at: exp })
+        structType = object.type.baseType
+      } else {
+        mustHaveAStructType(object, { at: exp })
+        structType = object.type
+      }
+      mustHaveMember(structType, id.sourceString, { at: id })
+      const field = structType.fields.find(f => f.name === id.sourceString)
+      return core.memberExpression(object, dot.sourceString, field)
+    },
+
+    Exp9_call(exp, open, expList, _close) {
+      const callee = exp.rep()
+      mustBeCallable(callee, { at: exp })
+      const exps = expList.asIteration().children
+      const targetTypes =
+        callee?.kind === "StructType"
+          ? callee.fields.map(f => f.type)
+          : callee.type.paramTypes
+      mustHaveCorrectArgumentCount(exps.length, targetTypes.length, { at: open })
+      const args = exps.map((exp, i) => {
+        const arg = exp.rep()
+        mustBeAssignable(arg, { toType: targetTypes[i] }, { at: exp })
+        return arg
+      })
+      return callee?.kind === "StructType"
+        ? core.constructorCall(callee, args)
+        : core.functionCall(callee, args)
+    },
+
+    Exp9_id(id) {
+      // When an id appears in an expression, it had better have been declared
+      const entity = context.lookup(id.sourceString)
+      mustHaveBeenFound(entity, id.sourceString, { at: id })
+      return entity
+    },
+
+    true(_) {
+      return true
+    },
+
+    false(_) {
+      return false
+    },
+
+    intlit(_digits) {
+      // Carlos ints will be represented as plain JS bigints
+      return BigInt(this.sourceString)
+    },
+
+    floatlit(_whole, _point, _fraction, _e, _sign, _exponent) {
+      // Carlos floats will be represented as plain JS numbers
+      return Number(this.sourceString)
+    },
+
+    stringlit(_openQuote, _chars, _closeQuote) {
+      // Carlos strings will be represented as plain JS strings, including
+      // the quotation marks
+      return this.sourceString
+    },
+  })
+
+  return builder(match).rep()
 }
